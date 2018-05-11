8881aa1775ac852da400b3f2ff09ca21818b925c
Javier LÒPEZ RAMOS <xavier.loos04@gmail.com>
2018-05-11 16:30:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,3 +6,9 @@ var height = "74"
 // Don't worry about this
 module.exports = { name, height
 }
+
+if (name==="dania"){
+  console.log("It is the real name")
+}else{
+  console.log("you are with the wrong person")
+}
